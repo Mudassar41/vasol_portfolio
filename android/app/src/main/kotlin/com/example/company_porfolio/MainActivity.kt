6cf813c2a46306d006e104fb09b5f512b723166b
unknown <you@example.com>
2022-06-28 13:49:59 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/company_porfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.company_porfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utills/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff022d62);
+  static const secondaryColor = Color(0xffef3139);
+}
